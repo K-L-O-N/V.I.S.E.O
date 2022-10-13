ff68f4fdd4b535fe9256ae2b069b72240b598831
jeonghyeok-Lee <85242295+jeonghyeok-Lee@users.noreply.github.com>
2022-10-13 09:01:51 +0900

    js file separation && cursor part separation

--- SmartMirrorModules/MMM-ShowAndHindMenu/MMM-ShowAndHind-Menu.js
+++ SmartMirrorModules/MMM-ShowAndHindMenu/MMM-ShowAndHind-Menu.js
@@ -1,23 +1,33 @@
-Module.register("MMM-ShowAndHind-Menu",{
+Module.register("MMM-ShowAndHind-Menu", {
 
-	config:null,	
-	
-	defaults : {
-		width: 200,													// 초기 가로 길이
-		height: 200,												// 초기 세로 길이
-		menuCount : 3,												// 클릭시 발생하는 첫 메뉴의 개수
-		mainMenuText : ["dress", "weather", "option"],				// 메뉴에 들어갈 텍스트
-		itemCount: [2,1,1],											// 메뉴별 항목의 초기 개수
+	config: null,
+
+	defaults: {
+		width: 200, // 초기 가로 길이
+		height: 200, // 초기 세로 길이
+		menuCount: 3, // 클릭시 발생하는 첫 메뉴의 개수
+		mainMenuText: ["dress", "weather", "option"], // 메뉴에 들어갈 텍스트
+		itemCount: [2, 1, 1], // 메뉴별 항목의 초기 개수
 		//message : "default message if none supplied in config.js",
-	},	
+	},
 
-	start : function(){
+	getStyles: function () {
+		return ["MMM-ShowAndHind-Menu.css"];
+	},
+
+	getScripts: function() {
+		return [
+			'functions.js'	
+		];
+	},
+
+	start: function () {
 		Log.info("Starting module: " + this.name);
 
 		// 자주 사용하기 위해서 세팅
 		const menuWidth = this.config.width;
 		const menuHeight = this.config.height;
-		const menuCount = this.config.menuCount; 
+		const menuCount = this.config.menuCount;
 		const menuContext = this.config.mainMenuText;
 		const itemCount = this.config.itemCount;
 
@@ -25,147 +35,94 @@ Module.register("MMM-ShowAndHind-Menu",{
 		let $body = document.querySelector("body");
 		let mainMenu = [];
 		let subMenu = [];
-		let showSubMenu = [3];		
-	
-		let isMenu = false;			// 초기 메뉴의 활성화 여부를 확인하기 위한 변수
-		let isMenuActive = false;		// 각 메뉴를 사용하고 있는지 확인하기 위한 함수
-		
-		let subMenuContainer;			// 메뉴의 아이템을 띄우는 공간을 만들어주기 위한 변수
-		let subMenuItemCount;			// 클릭된 메뉴의 아이템의 개수를 파악하기 위한 변수
-		let next;						// 다음 버튼 생성용 변수
+		let showSubMenu = [3];
+
+		let isMenu = false; 	// 초기 메뉴의 활성화 여부를 확인하기 위한 변수
+		let isMenuActive = false; // 각 메뉴를 사용하고 있는지 확인하기 위한 함수
+
+		let subMenuContainer; 	// 메뉴의 아이템을 띄우는 공간을 만들어주기 위한 변수 - 일단 현재는 필요가 없음
+		let subMenuItemCount; 	// 클릭된 메뉴의 아이템의 개수를 파악하기 위한 변수
+		let next; 				// 다음 버튼 생성용 변수
 		let prev;
 
+		let ripples; 			// 마우스 클릭 시 물결을 표시하기 위한 변수
+
 		console.log(itemCount);
-		/** 
-		 * 화면에 나오는 모듈을 숨기는 함수
-		 * @param {string} moduleName 숨기고자하는 모듈의 이름 작성  
-		 */
-		function hind(moduleName){	// module hind
-			MM.getModules().withClass([`${moduleName}`]).enumerate(function(module){module.hide();});
-		}
-
-		/** 
-		 * 화면에 숨겨진 모듈을 보여주는 함수
-		 * @param {string} moduleName 보여주고자 하는 모듈의 이름 작성  
-		 */
-		function show(moduleName){	// module show
-			MM.getModules().withClass([`${moduleName}`]).enumerate(function(module){module.show();});
-		}
-
-		/**
-		 * 마우스 커서 설정
-		 */
-		function setCursor(obj, mouseCursor){
-			obj.style.cursor = `${mouseCursor}`;
-		}
-
-/*
-		function showListReserch(x){
-			for(let i = 0; i < parseInt(x); i++){
-				showSubMenu[i] = subMenu[`${i+x}`];
-			}
-			for(let i = 0; i < 3; i++){
-				showSubMenu[i].style.top = `50%`;
-				showSubMenu[i].style.left = `(75-${25*i})%`;
-				showSubMenu[i].style.transfrom = `translate(-50%, -50%)`;
-				$body.appendChild(showSubMenu[i]); 	
-			}
-		}
-*/
-
-		/**
-		 * 메뉴를 생성하는 데 필요한 div를 만들기 위한 함수
-		 * @param {string} context 메뉴에 표시될 문자열
-		 * @param {number} width 메뉴의 가로 크기
-		 * @param {number} height 메뉴의 세로 크기
-		 */
-		function createDiv(context, width, height){	
-			var div = document.createElement('div');
-			div.innerHTML = `${context}`;
-			div.style.position = 'absolute';
-			div.style.width = `${width}px`;
-			div.style.height = `${height}px`;
-			div.style.textAlign = 'center';
-			div.style.lineHeight = `${height}px`;
-			div.style.border = '5px solid white';
-			div.style.borderRadius = '10px';
-			div.style.onmouseover= `${setCursor(div,'pointer')}`;
-			return div;
-		}
-
 		/**
 		 * 초기 메뉴를 제거하는 함수
 		 * 메뉴 활성화 여부 true -> false / 자식객체 삭제
 		 */
-		function hindMenu(){					// delete menu item
+		function hindMenu() { // delete menu item
 			isMenu = false;
 			show('newsfeed');
-			for(let i = 0; i< menuCount; i++){
-				$body.removeChild(mainMenu[i]);		
+			for (let i = 0; i < menuCount; i++) {
+				$body.removeChild(mainMenu[i]);
 			}
 		}
 
 		/**
 		 * 메뉴의 아이템을 제거하는 함수
 		 * 메뉴 활성화 여부 true -> false / 자식객체 삭제 
+		 * @param {string} index 서브 메뉴의 아이템 수를 문자열로 가져옴
 		 */
-		function hindSubMenu(index){				
+		function hindSubMenu(index) {
 			isMenuActive = false;
 			show('calendar');
 			show('clock');
 			show('weather');
-			show('MMM-AirQuality');		
+			show('MMM-AirQuality');
 
 			$body.removeChild(next);
 			$body.removeChild(prev);
 
-			for(let i = 0; i< itemCount[parseInt(index)]; i++){
+			for (let i = 0; i < itemCount[parseInt(index)]; i++) {
 				//subMenuContainer.removeChild(subMenu[i]);
-
 				$body.removeChild(subMenu[i]);
 			}
-	
+
 			console.log(subMenu);
-			console.log("index"+index)
+			console.log("index" + index)
 			//$body.removeChild(subMenuContainer);	
 		}
-		
+
 		/**
 		 * 메뉴를 생성
 		 */
-		function createMenu(){
-			if(!isMenu && !isMenuActive){
+		function createMenu() {
+			if (!isMenu && !isMenuActive) {
 				isMenu = true;
 				hind('newsfeed');
-				for(let i = 0; i< menuCount; i++){
+				for (let i = 0; i < menuCount; i++) {
 					// make menu
-					mainMenu[i] = createDiv(menuContext[i],menuWidth,menuHeight);
+					mainMenu[i] = createDiv(menuContext[i], menuWidth, menuHeight);
 					mainMenu[i].style.top = '90%';
-					if(menuCount == 2){
+					if (menuCount == 2) {
 						mainMenu[i].style.left = `${25 + i*50}%`;
-					}else if(menuCount == 3){
+					} else if (menuCount == 3) {
 						mainMenu[i].style.left = `${25 + i*25}%`;
-					}else{
+					} else {
 						mainMenu[i].style.left = '50%';
 					}
 					mainMenu[i].style.transform = 'translate(-50%,-50%)';
-					mainMenu[i].addEventListener('click',function(){ // subMenuEvent 
+					mainMenu[i].addEventListener('click', function () { // subMenuEvent 
 						subMenuEvent(i);
-					},false);  
+					}, false);
 					$body.appendChild(mainMenu[i]);
 				}
 			}
 		}
-			
+
 		/**
 		 * 서브 메뉴를 생성
 		 * @param {number} index 해당 메뉴에서 보여줘야 할 아이템 개수
-		 */	
-		function subMenuEvent(index){
+		 */
+		function subMenuEvent(index) {
 			hindMenu();
 			isMenuActive = true;
-			
 			subMenuItemCount = index;
+
+			let showItemIndex = 0;		// 현재 중앙에 보여주고 있는 아이템의 인덱스 번호
+			let clickOption = 1;		// 현재 클릭한 옵션이 무엇인지 확인하기 위한 변수 next => 양수 / prev => 음수
 
 			/*
 			subMenuContainer = createDiv('container', 700,700);
@@ -176,43 +133,81 @@ Module.register("MMM-ShowAndHind-Menu",{
 
 			//$body.appendChild(subMenuContainer);
 
-			for(let i = 0; i < itemCount[parseInt(index)]; i++){
-				subMenu[i] = createDiv(i,menuWidth,menuHeight);
+			for (let i = 0; i < itemCount[parseInt(index)]; i++) {
+				subMenu[i] = createDiv(i, menuWidth, menuHeight);
 				subMenu[i].style.top = `${50}%`;
-				if(itemCount[parseInt(index)] == 2){
-					subMenu[i].style.left = `${50 - i*25}%`;
-				}else{
-					subMenu[i].style.left = '50%';
+				if (i == 2){
+					subMenu[i].style.left = `${75 - i*25}%`;
+				}else if (i == 1) {
+					subMenu[i].style.left = `${75 - i*25}%`;
+				} else {
+					subMenu[i].style.left = '75%';
 				}
 				subMenu[i].style.transform = `translate(-50%,-50%)`;
 				//subMenuContainer.appendChild(subMenu[i]);
-				$body.appendChild(subMenu[i]);
-			}			
 
-/*
+/* 				
+				// 현재 테스트 중임 아래 방법으로는 안움직임
+				const keyframe = document.createElement('style');
+				keyframe.innerHTML = `  
+					@keyframes move {
+						0% {
+							left = ${subMenu[i].style.left}%;
+						}
+						100% {
+							left = ${subMenu[i].style.left} + 25 * ${clickOption}%;
+						}
+					}`;
+
+				subMenu[i].appendChild(keyframe);
+				subMenu[i].animation = 'move 1s linear'; */
+
+				$body.appendChild(subMenu[i]);
+			}
+
+			/*
 			for(let i = 0; i < itemCount[parseInt(index)]; i++){
 				subMenu[i] = createDiv(i,menuWidth,menuHeight);
 			}
-			
+						
 			showListReserch(0);
-*/
+			*/
 
-			next = createDiv('next' , 100,100);
+
+			next = createDiv('next', 100, 100);
 			next.style.top = '50%';
 			next.style.left = '90%';
 			next.style.transform = `translate(-50%,-50%)`;
-			next.addEventListener('click',function(){ // subMenuEvent 
-						console.log('next click');
-					},false);  
 
-			prev = createDiv('prev' , 100,100);
+			next.addEventListener('click', function () { // subMenuEvent 
+				if(itemCount[parseInt(index)] >= showItemIndex){
+					console.log('다음으로 넘어갈 수 있음');
+					console.log(subMenu[showItemIndex].style.left);
+					subMenu[showItemIndex].style.left += '25%';
+					showItemIndex++;
+					clickOption = 1;
+				}else{
+					console.log('다음으로 갈 수가 없음');
+				}
+			}, false);
+
+			prev = createDiv('prev', 100, 100);
 			prev.style.top = '50%';
 			prev.style.left = '10%';
 			prev.style.transform = `translate(-50%,-50%)`;
-			prev.addEventListener('click',function(){ // subMenuEvent 
-						console.log('prev click');
-					},false); 
+			prev.addEventListener('click', function () { // subMenuEvent 
+				console.log('prev click');
+				if(itemCount[parseInt(index)] >= showItemIndex && showItemIndex >= 0){
+					console.log('이전으로 갈 수 있음');
+					showItemIndex--;
+					clickOption = -1;
+					subMenu[showItemIndex].style.left -= '25%';
+				}else{
+					console.log('이전으로 갈 수가 없음');
+				}
+			}, false);
 			
+			console.log("showItemIndex : "+showItemIndex);
 			$body.appendChild(next);
 			$body.appendChild(prev);
 
@@ -222,40 +217,41 @@ Module.register("MMM-ShowAndHind-Menu",{
 			hind('MMM-AirQuality');
 		}
 
+
 		$body.addEventListener('click', createMenu, false);
-		
+
 		// keycode => 27 (ESC) 
-		window.addEventListener('keydown', (e) =>{
-			if(e.keyCode == 27 && isMenu) {
+		window.addEventListener('keydown', (e) => {
+			if (e.keyCode == 27 && isMenu) {
 				hindMenu();
-			}
-			else if(e.keyCode == 27 && isMenuActive) {
+			} else if (e.keyCode == 27 && isMenuActive) {
 				hindSubMenu(subMenuItemCount);
 				createMenu();
 			}
 		});
-	
+
 		// 30초간 아무런 움직임이 없을 때, 메뉴가 활성화 되어 있다면 해당 메뉴를 제거
-		setInterval(function(){
-			if(isMenu) hindMenu();
-			if(isMenuActive) hindSubMenu(subMenuItemCount);
-		}, 60000); 
+		setInterval(function () {
+			if (isMenu) hindMenu();
+			if (isMenuActive) hindSubMenu(subMenuItemCount);
+		}, 60000);
 	},
 
-	notificationReceived: function(notification, payload, sender){
+
+	notificationReceived: function (notification, payload, sender) {
 		//if(notification ==="All modules started!") this.sendSocketNotification("CONFIG",this.config);
 		//if(sender) Log.log(this.name + " received a module notification: " + notification + " form sender: " + sender.name + " check : " + payload);
 		//else Log.log(this.name + "received a system notification: " + notification);
 	},
 
 	socketNotificationReceived: function (notification, payload) {
-/*		Log.log(this.name + " received a socket notification: " + notification + " - payload: " +  payload);
-		switch(notification){
-			case "message_from_helper":
-				this.config.message = payload;
-				this.updateDom(1000);
-				break;
-		}
-*/
-	},		
+		/*		Log.log(this.name + " received a socket notification: " + notification + " - payload: " +  payload);
+				switch(notification){
+					case "message_from_helper":
+						this.config.message = payload;
+						this.updateDom(1000);
+						break;
+				}
+		*/
+	},
 });
