66b0644362d0184689eefdead0f10c3ab4a78b5e
jeonghyeok.lee <jeonghyeok.lee.dev@gmail.com>
2022-11-16 00:19:30 +0900

    feat(Dress):Add event listener for testing
    추후 마우스 hover시 확대되는 이벤트 추가를 위해서 테스트용 이벤트 설정

--- SmartMirrorModules/MMM-Dress/MMM-Dress.js
+++ SmartMirrorModules/MMM-Dress/MMM-Dress.js
@@ -19,13 +19,18 @@ Module.register("MMM-Dress", {
         let div = document.createElement("img");
         div.classList.add("test");
         div.src="modules/MMM-Dress/2.png";
-        div.width=100;
-        div.height=100;
+        div.width= 100;  
+        div.height= 100;
 
         div.addEventListener('click', (e)=>{
             console.log("클릭되었습니다.");
         });
-        
+        div.addEventListener('mouseover', (e)=>{
+            console.log("마우스가 올라왔습니다.");
+            div.width=120;
+            div.height=120;
+        });
+
         wrapper.appendChild(div);
         return wrapper;
     },
