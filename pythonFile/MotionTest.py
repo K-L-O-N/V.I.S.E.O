b76086f468fe80b503ea275e4184746bf9f991fa
Marlin-1013 <84124723+Marlin-1013@users.noreply.github.com>
2022-11-21 23:09:05 +0900

    esc버튼, 화면지우기

--- pythonFile/MotionTest.py
+++ pythonFile/MotionTest.py
@@ -13,7 +13,6 @@ def main():
     wScr, hScr = ap.screen.size()
     clocX, clocY = 0, 0
     plocX, plocY = 0, 0
-    frameR = 200
     smoothening = 10
     wCam, hCam = 1280, 720
     isReady = False
@@ -22,7 +21,8 @@ def main():
     cap = cv2.VideoCapture(0)
     cap.set(3,wCam)
     cap.set(4,hCam)
-    count = 0
+    Ccount = 0
+    Ecount = 0
 
     while True:
         ret, frame = cap.read()
@@ -49,29 +49,37 @@ def main():
 
 
         if isMenu:
-            cv2.putText(frame, "Menu Field", (frameR, frameR-5), 0, 2, (0, 255, 0), 3)
-            cv2.rectangle(frame, (frameR, frameR), (wCam - frameR, hCam - frameR),
-                          (255, 0, 255), 2)
-            if fingers == [1, 1, 0, 0, 0] or count == 1:
-                x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
-                y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+            # cv2.putText(frame, "Menu Field", (300, 240), 0, 2, (0, 255, 0), 3)
+            # cv2.rectangle(frame, (300, 250), (wCam - 400, hCam - 250),
+            #               (255, 0, 255), 2)
+            if fingers == [1, 1, 0, 0, 0] or Ccount == 1:
+                x3 = np.interp(x1, (300, wCam - 400), (0, wScr))
+                y3 = np.interp(y1, (250, hCam - 250), (0, hScr))
                 clocX = plocX + (x3 - plocX) / smoothening
                 clocY = plocY + (y3 - plocY) / smoothening
                 # 마우스 움직임
                 ap.mouse.move(wScr - clocX, clocY)
-                cv2.circle(frame, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+                # cv2.circle(frame, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
                 plocX, plocY = clocX, clocY
 
             #마우스 토글
             if fingers[0] == 1 and fingers[1] == 0:
-                if count < 1:
+                if Ccount < 1:
                     ap.mouse.toggle(ap.mouse.Button.LEFT, True)
-                    cv2.circle(frame, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
-                    count += 1
+                    # cv2.circle(frame, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+                    Ccount += 1
             else:
-                if count > 0:
+                if Ccount > 0:
                     ap.mouse.toggle(ap.mouse.Button.LEFT, False)
-                    count = 0
+                    Ccount = 0
+
+            if fingers[0] == 0 and fingers[1] == 1:
+                if Ecount < 1:
+                    ap.key.tap(ap.key.Code.ESCAPE)
+                    Ecount += 1
+            else:
+                if Ecount > 0:
+                    Ecount = 0
 
             if fingers == [0, 0, 0, 0, 0]:
 
@@ -81,22 +89,23 @@ def main():
 
 
 
-        if fingers == [0, 0, 0, 0, 0]:
-            cv2.putText(frame, "Close", (bbox[0], bbox[3]), 0, 2, (0, 255, 0), 3)
-        elif fingers == [1, 1, 1, 1, 1]:
-            cv2.putText(frame, "Open", (bbox[0], bbox[3]), 0, 2, (0, 255, 0), 3)
-        elif fingers == [1, 1, 0, 0, 0]:
-            cv2.putText(frame, "Move", (bbox[0], bbox[3]), 0, 2, (0, 255, 0), 3)
-        elif fingers == [1, 0, 0, 0, 0]:
-            cv2.putText(frame, "Click", (bbox[0], bbox[3]), 0, 2, (0, 255, 0), 3)
+        # if fingers == [0, 0, 0, 0, 0]:
+        #     cv2.putText(frame, "Close", (bbox[0], bbox[3]), 0, 2, (0, 255, 0), 3)
+        # elif fingers == [1, 1, 1, 1, 1]:
+        #     cv2.putText(frame, "Open", (bbox[0], bbox[3]), 0, 2, (0, 255, 0), 3)
+        # elif fingers == [1, 1, 0, 0, 0]:
+        #     cv2.putText(frame, "Move", (bbox[0], bbox[3]), 0, 2, (0, 255, 0), 3)
+        # elif fingers == [1, 0, 0, 0, 0]:
+        #     cv2.putText(frame, "Click", (bbox[0], bbox[3]), 0, 2, (0, 255, 0), 3)
 
-        cv2.imshow('test', frame)
+        # cv2.imshow('test', frame)
         keycode = cv2.waitKeyEx(1)
         if keycode == 0x240000:
             print("OK")
         elif keycode == 0x230000:
             print("end")
-
+        elif keycode == 0x1B:
+            print("esc")
 
 
 if __name__ == "__main__":
